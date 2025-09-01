64e4c2f6271ea7ba9d4feb8029781f8ce88a3ab8
ZephyrSober <123217851+ZephyrSober@users.noreply.github.com>
2025-09-01 21:43:19 +0800

    完成第一章作业

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,34 @@
-#include <printf.h>
+#include <stdio.h>
+#include <windows.h>
+
+#define WIDTH 50
+#define FORWARD 1
+#define BACKWARD -1
+
+void run_letter(const int direction)
+{
+    for (int position = (direction>0)?0:WIDTH; position != (WIDTH - ((direction>0)?0:WIDTH)); position+=direction)
+    {
+        system("cls");
+        for (int i = 0; i != WIDTH; i++)
+        {
+            if (i == position)
+            {
+                printf("a");
+            }else
+            {
+                printf(" ");
+            }
+        }
+        Sleep(100);
+    }
+}
 
 int main() {
-    printf("hello world!\n");
+    while (1)
+    {
+        run_letter(FORWARD);
+        run_letter(BACKWARD);
+    }
     return 0;
 }
