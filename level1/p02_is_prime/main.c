ba73fb90274c915e430fcd76aacdb22c4a858990
ZephyrSober <123217851+ZephyrSober@users.noreply.github.com>
2025-09-02 13:05:50 +0800

    完成第二章作业

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,32 @@
-#include <printf.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+int get_sqrt(const int n)  //get the
+{
+    double d_n = n;
+    int target = (int) sqrt(d_n);
+    return target;
+}
+
+bool is_prime(int n)
+{
+    for (int i = 2;i !=get_sqrt(n)+1;i++)
+    {
+        if (n%i == 0)
+        {
+            printf("%d is not prime",n);
+            return false;
+        }
+    }
+    printf("%d is prime",n);
+    return true;
+}
 
 int main() {
-    printf("hello world!\n");
+    int n;
+    printf("input number");
+    scanf("%d",&n);
+    is_prime(n);
     return 0;
 }
